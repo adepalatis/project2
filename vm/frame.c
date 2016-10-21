0da10af312ab29387799f4a4b8f47abdc31186b9
Tony DePalatis <111tdepalatis@gmail.com>
2016-10-21 02:54:26 +0100

    Finished (first iteration of) get_frame_multiple

--- vm/frame.c
+++ vm/frame.c
@@ -9,18 +9,37 @@ struct frame {
 	bool pinned;
 };
 
-struct frame* palloc_get_multiple_frames(enum palloc_flags flags, size_t frame_cnt) {
+struct frame* get_multiple_frames(enum palloc_flags flags, size_t frame_cnt) {
 	struct pool* pool = flags & PAL_USER ? &user_pool : &kernel_pool;
-	
+	void* frames;
+	size_t frame_idx;
+
 	if(frame_cnt == 0) {
 		return NULL;
 	}
 
-	// lock_acquire(&pool->lock);
+	lock_acquire(&pool->lock);
+	frame_idx = bitmap_scan_and_flip(pool->used_map, 0, frame_cnt, false);
+	lock_release(&pool->lock);
 
+	if(frame_idx != BITMAP_ERROR) {
+		frames = pool->base + PGSIZE * frame_idx;
+	} else {
+		frames = NULL;
+	}
+
+	if(frames != NULL) {
+		if(flags & PAL_ZERO) {
+			memset(frames, 0, PGSIZE * frame_cnt);
+		}
+	} else if(flags & PAL_ASSERT) {
+		PANIC("palloc_get: out of frames");
+	}
+
+	return frames;
 }
 
-struct frame* palloc_get_frame(enum palloc_flags flags) {
-	return palloc_get_multiple_frames(flags, 1);
+struct frame* get_frame(enum palloc_flags flags) {
+	return get_multiple_frames(flags, 1);
 }
 
--- vm/frame.h
+++ vm/frame.h
@@ -4,7 +4,7 @@
 #include "threads/palloc.h"
 #include "threads/palloc.c"
 
-struct frame* palloc_get_multiple_frames(enum palloc_flags flags, size_t frame_cnt);
-struct frame* palloc_get_frame(enum palloc_flags flags);
+struct frame* get_multiple_frames(enum palloc_flags flags, size_t frame_cnt);
+struct frame* get_frame(enum palloc_flags flags);
 
 #endif /* vm/frame.h */
