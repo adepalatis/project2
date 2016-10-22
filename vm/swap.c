2ab5d5509858ba78d5c2ff681e7e726a35bb4352
Tony DePalatis <111tdepalatis@gmail.com>
2016-10-22 20:20:28 +0100

    Created the global swap table as well as a function stub for its initializer

--- vm/swap.c
+++ vm/swap.c
@@ -1,7 +1,7 @@
 #include "vm/swap.h"
 
-struct ste {
-	struct thread* thread;
-	void* page;
-	int swap_idx;
+struct ste swap_table[1024];
+
+void swap_table_init(void) {
+
 }
--- vm/swap.h
+++ vm/swap.h
@@ -1,4 +1,14 @@
 #ifndef SWAP_H 
 #define SWAP_H 
 
+#include "devices/block.h"
+
+struct ste {
+	struct thread* thread;
+	void* page;
+	int swap_idx;
+};
+
+void swap_table_init(void);
+
 #endif /* vm/swap.h */
